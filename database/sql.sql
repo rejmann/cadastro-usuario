fee1b949e08300039de71a93564d6f3fd73e8928
Rejman <rejmans00@gmail.com>
2023-04-03 18:49:36 -0300

    banco de dados

---
+++ database/sql.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `cadastro-usuario`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+CREATE TABLE `cadastro-usuario`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
