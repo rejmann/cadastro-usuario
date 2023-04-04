6d874ef96452f1c9dc078620285bfec2c8e2a79e
Rejman <rejmans00@gmail.com>
2023-04-03 21:30:21 -0300

    criando banco de dados

--- database/sql.sql
+++ database/sql.sql
@@ -1,11 +1,10 @@
-CREATE TABLE `cadastro-usuario`.`usuario` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `senha` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
-CREATE TABLE `cadastro-usuario`.`usuario` (
+DROP DATABASE IF EXISTS `cadastro_usuario`;
+CREATE DATABASE `cadastro_usuario`;
+USE `cadastro_usuario`;
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
